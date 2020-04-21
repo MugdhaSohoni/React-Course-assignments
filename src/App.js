7d5e0517901ece2914193dcebc82f8e20f2a262b
MugdhaSohoni <57430277+MugdhaSohoni@users.noreply.github.com>
2020-04-21 15:24:27 +0530

    Uploading the src folder

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import ValidationComponent from './ValidationComponent/ValidationComponent';
+import CharComponent from './CharComponent/CharComponent';
+
+class App extends Component {
+  state = {
+    text: "Hello"
+  }
+
+  textHandler = (event) => {
+    this.setState({text: event.target.value});
+  }
+
+  deleteCharHandler = (index) => {
+    const chars = [...this.state.text.split("")];
+    chars.splice(index, 1);
+    this.setState({text: chars.join("")});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <input type="text" onChange={this.textHandler} value={this.state.text}></input>
+          <p>{this.state.text.length} chars</p>
+          <ValidationComponent text={this.state.text} />
+          <div>
+            {this.state.text.split("").map((char, index) => {
+              return <CharComponent char={char} delete={() => this.deleteCharHandler(index)} />
+            })}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
